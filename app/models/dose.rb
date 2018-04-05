436a21347499efa7309e01e5f0cf6594baeb0b39
Jane Wong <jwly1024@gmail.com>
2018-04-05 15:14:09 +0800

    finished creating controllers

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates_uniqueness_of :ingredient_id, :scope => [:ingredient_id, :cocktail_id]
+  validates_uniqueness_of :ingredient_id, :scope => [ :cocktail_id ]
   validates :description, presence: true
 end
